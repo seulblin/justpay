f2af2d7c3d8aced7294509512946c324ac9e16b4
seulblin <subinge94@gmail.com>
2021-11-29 10:06:49 +0900

    오랜만에 올리기

--- js/js.js
+++ js/js.js
@@ -80,6 +80,39 @@ $(document).ready(function(){
     });
     });
 
+    //탭 안에 슬라이드
+    $('button').click(function(){
+        var $this = $(this);
+        var index = $this.index();
+        
+        $this.addClass('active');
+        $this.siblings('button.active').removeClass('active');
+        
+        var $outer = $this.closest('.outer');
+        var $current = $outer.find(' > .tabs > .tab.active');
+        var $post = $outer.find(' > .tabs > .tab').eq(index);
+        
+        $current.removeClass('active');
+        $post.addClass('active');
+        // 위의 코드는 탭메뉴 코드입니다.
+        
+        $('.slider').slick('setPosition');
+        // 탭 페이지 안에서 slick 사용시 – slick이 첫페이지에 있지 않으면 slick의 첫번째 이미지가 보이지 않고 2번째 부터 도는것을 확인 할 수 있다. 해당 문제는 탭이 active가 된 후 그 페이지에 slick이 있다면 = slick의 위치를 수동으로 새로 고쳐줘야 한다.
+    });
+    
+    // 기존 처음의 slick 적용
+    $('.slider').slick({
+        slidesToShow: 3,
+        slidesToScroll : 1,
+        infinite : true, 
+        autoplay : true,			
+		autoplaySpeed : 1000, 	
+        pauseOnHover : false,
+        dots: false,
+    });
+
+
+
 
 });
 
@@ -95,5 +128,36 @@ function agree(){
     }
 }
 
-
+function getDate(date) {
+    return date.toLocaleDateString().replace(/\./g, "").split(" ");
+  }
+  
+  const pad = (str) => str > 10 ? str : '0' + str;
+  
+  window.onload = function() {
+    const ToDay = new Date();
+    
+    const nowMonth = ToDay.getMonth();
+    
+    const [y, m] = getDate(new Date(ToDay.setMonth(nowMonth)));
+  
+    const lastDay = getDate(new Date(y, m, 0)).pop() * 1;
+    
+    const day = new Date([y, m, 1].join("-")).getDay() * 1;
+    
+    const maxDay = Math.ceil((day + lastDay) / 7) * 7;
+  
+    let html = '';
+  
+    for (let i = 1; i <= maxDay; i++) {
+      const diff = i - day;
+      const d = diff <= lastDay && i > day ? diff : '';
+      const tmpClass = !d ? 'background' : '';
+  
+      html += `<div class="dateSel ${tmpClass}">${d}</div>`;
+    }
+  
+    document.querySelector('.dateSel').innerHTML = html;
+    document.querySelector('.date_text').innerText = `${y}년 ${pad(m)}월`;
+  }
 
